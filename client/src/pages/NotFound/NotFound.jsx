157b355814c6a8f0cbd5f1abd83803c5ec83a602
Cameron Keokolo Tabion <cameronkeokolotabion@gmail.com>
2021-03-14 21:42:19 -0700

    Fixed not found page

--- client/src/pages/NotFound/NotFound.jsx
+++ client/src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => (
-  <h1>
-    NotFound
-  </h1>
+  <div className="not-found">
+    <h1>Cannot find the page you are looking for!</h1>
+    <Link to="/">Click to go back</Link>
+  </div>
 );
 
 export default NotFound;
