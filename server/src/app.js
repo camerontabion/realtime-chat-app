78bd289a38001d18b25403a60304148ed4fead63
Cameron Tabion <cameronkeokolotabion@gmail.com>
2021-04-01 02:32:11 -0700

    Removed unused import

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import { createServer } from 'http';
-import { Server } from 'socket.io';
 import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
